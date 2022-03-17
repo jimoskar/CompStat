5987e6cacd5464ad4c6fd59580800e92d2eec9c6
martintufte <martingudahltufte@hotmail.com>
2022-03-17 10:39:02 +0100

    YOLO

--- ex2/problem1.R
+++ ex2/problem1.R
@@ -156,33 +156,15 @@ load("rain.rda")
 # sample autocorrelation
 source('sacf.R')
 
-T <- 366
-n <- rep(39, T)
-n[60] = 10
-alpha <- 2
-beta <- 0.05
-
-# response
-y <- rain$n.rain
-# Construct Q (without the 1/sigma2 factor)
-Q <- diag(rep(2, T))
-Q[row(Q) - col(Q) == 1] <-  Q[row(Q) - col(Q) == -1] <- -1
-Q[1,1] <- Q[T,T] <- 1
-
-num.iter <- 500 # 50000 will take about 30 min on my system
-
-
 # "expit"/"sigmoid" function
 expit <- function(x){
   1/(exp(-x)+1)
 }
 
-
 # Negative logarithmic "expit"
 neg.log.expit <- function(x){
   log(exp(-x)+1)
 }
-
 
 # Function to sample from inverse gamma
 rigamma <- function(n, shape, scale){
@@ -190,15 +172,25 @@ rigamma <- function(n, shape, scale){
   return(1/rgamma(n, shape = shape, rate = 1/scale))
 }
 
-tau.accept <- function(t, tau.prop, tau.curr){
+tau.accept <- function(t, tau.prop, tau.curr, y, n){
   log.acc <- y[t] * ( neg.log.expit(tau.curr) - neg.log.expit(tau.prop) ) +
       (y[t] - n[t]) * ( neg.log.expit(-tau.curr) - neg.log.expit(-tau.prop) )
+  
+  min(1, exp(log.acc))
+}
+
+tau.accept.block <- function(I, tau.prop, tau.curr){
+  log.acc <- 0
+  for(t in I){
+    log.acc <- log.acc + y[t] * ( neg.log.expit(tau.curr) - neg.log.expit(tau.prop) ) +
+      (y[t] - n[t]) * ( neg.log.expit(-tau.curr) - neg.log.expit(-tau.prop) )
+  }
   
   exp(log.acc)
 }
 
 
-MCMCMC <- function(n.iter, tau, sigma2){
+MCMCMC <- function(n.iter, tau, sigma2, Q, y, n){
   # length of tau
   T <- length(tau)
   
@@ -218,23 +210,30 @@ MCMCMC <- function(n.iter, tau, sigma2){
       # tau.A conditioned on tau.B
       mu.cond <- -1/Q.AA * Q.AB %*% tau.mat[mt, i]
       Q.cond <- Q.AA
+      #print(paste("mu: ", mu.cond))
+      #print(paste("Q: ", Q.cond))
       
       # Generate proposal
-      tau.proposal <- rnorm(1, as.numeric(mu.cond), sigma2.vec[i]/Q.cond)
+      tau.proposal <- rnorm(1, mean = mu.cond, sd = sqrt(sigma2.vec[i]/Q.cond))
+      
+      #print('Iteration:')
+      #print(tau.mat[t,i])
+      #print(tau.proposal)
       
       # Calculate acceptance probability
-      alpha <- tau.accept(t, tau.proposal, tau.mat[t,i])
+      acc <- tau.accept(t, tau.proposal, tau.mat[t,i], y, n)
+      if(is.na(acc)){browser()}
+      #print(acc)
       
       # Draw from uniform distribution
       u <- runif(1)
-      if(u < alpha){
+      if(u < acc){
         tau.mat[t, i+1] = tau.proposal
       } else{
         tau.mat[t, i+1] = tau.mat[t, i]
       }
     }
     # Gibbs step for sigma2
-    
     shape <- alpha + (T-1)/2
     scale <- 0.5*t(tau.mat[,i]) %*% Q %*% tau.mat[, i] + beta
     sigma2.vec[i+1] <- rigamma(1, shape = shape, scale = scale)
@@ -243,19 +242,43 @@ MCMCMC <- function(n.iter, tau, sigma2){
   list("tau" = tau.mat, "sigma2" = sigma2.vec)
 }
 
-# set some initial values
-tau.0 <- runif(366, min=-3, max=0)
-pi.0 <- expit(tau.0)
-sigma2.0 <- 0.2
 
-# test the MCMC algorithm
-time <- proc.time()
-run <- MCMCMC(10000, tau.0, sigma2.0)
-print(proc.time()-time)
+# Test if the acceptance rate is wrong.
+do.iterations <- function(n.iter = 50000){
+  T <- 366
+  n <- rep(39, T)
+  n[60] = 10
+  #y = rain$n.rain
+  y <- rain$n.rain
+  
+  alpha <- 2
+  beta <- 0.05
+  
+  # Construct Q (without the 1/sigma2 factor)
+  Q <- diag(rep(2, T))
+  Q[row(Q) - col(Q) == 1] <-  Q[row(Q) - col(Q) == -1] <- -1
+  Q[1,1] <- Q[T,T] <- 1
+  
+  # initial values
+  tau.0 <- runif(366, min=-3, max=0)
+  sigma2.0 <- 0.2
+  
+  # do the MCMC calculations
+  time <- proc.time()
+  run <- MCMCMC(n.iter, tau.0, sigma2.0, Q, y, n)
+  print(proc.time()-time)
+  
+  run
+}
 
-hist(tail(1/run$sigma2, 500), freq = FALSE)
-plot(run$tau[201,], type = 'l')
+run <- do.iterations(n.iter=2)
+
+hist(tail(run$sigma2, 100), freq = FALSE)
+plot(run$tau[100,], type = 'l')
 
 
-length(run$tau[, 1])
+
+
+
+
 
