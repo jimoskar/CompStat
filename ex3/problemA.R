8cf6ce4f4a24ce8419a7c7e78dbcbde716714a51
Martin Gudahl Tufte <martingudahltufte@hotmail.com>
2022-04-25 10:43:12 +0200

    Add changes to problem A.

--- ex3/problemA.R
+++ ex3/problemA.R
@@ -1,10 +1,112 @@
 ### Problem A ----
 library(ggplot2)
 library(MASS)
-library(coda)
-source('MCMC.R')
-source('sacf.R')
+source('files/probAhelp.R')
+source('files/probAdata.R')
+
+## 1. ----
+
+## Multiple residual bootstraping
+ARp.beta.bootstrap <- function(x, p, B){
+  # Fit the regression model
+  beta <- ARp.beta.est(x, p)
+  T <- length(x)
+  
+  # Find the residuals
+  resid.LS <- ARp.resid(x, beta$LS)
+  resid.LA <- ARp.resid(x, beta$LA)
+  
+  # Initialize matrices to store the bootstrap estimates of beta
+  betas.LS <- matrix(NA, nrow=B, ncol=p)
+  betas.LA <- matrix(NA, nrow=B, ncol=p)
+  
+  for(b in 1:B){
+    # Find consequetive starting values for the resampling
+    idx <- sample(1:(T-p+1), 1)
+    x0 <- x[idx:(idx+p-1)]
+    
+    # Resample residuals and x
+    resample.resid.LS <- sample(resid.LS, replace = TRUE)
+    resample.resid.LA <- sample(resid.LA, replace = TRUE)
+    resample.x.LS <- ARp.filter(x0, beta$LS, resample.resid.LS)
+    resample.x.LA <- ARp.filter(x0, beta$LA, resample.resid.LA)
+    
+    # Store new estimates for beta
+    betas.LS[b,] <- ARp.beta.est(resample.x.LS, p)$LS
+    betas.LA[b,] <- ARp.beta.est(resample.x.LA, p)$LA
+  }
+  return(list(LS=betas.LS, LA=betas.LA))
+}
+
+# Time series of interest
+x <- data3A$x
+p <- 2
+beta <- ARp.beta.est(x, p)
+
+# Plot x
+ts <- ggplot(data.frame(x=1:100, y=x), aes(x=x, y=y)) +
+  geom_line(lwd=0.5) + labs(x="x", y="y") + theme_bw()
+ggsave("./figures/x.pdf", plot = ts, width = 4, height = 2)
 
 
-## a) ----
+B <- 2000
+set.seed(4200)
+betas <- ARp.beta.bootstrap(x, p, B)
+
+## Estimate variance of the two estimators
+var.LS <- apply(betas$LS, 2, var)
+var.LA <- apply(betas$LA, 2, var)
+
+# Estimated bias of the two samples using the bootstrap expectation
+bias.LS <- apply(betas$LS, 2, mean) - beta$LS
+bias.LA <- apply(betas$LA, 2, mean) - beta$LA 
+
+
+
+## 2. ----
+
+ARp.pred.bootstrap <- function(x, p, B){
+  # Fit the regression model
+  beta <- ARp.beta.est(x, p)
+  T <- length(x)
+  
+  # Find the residuals
+  resid.LS <- ARp.resid(x, beta$LS)
+  resid.LA <- ARp.resid(x, beta$LA)
+  
+  # Initialize matrices to store the bootstrap estimates of x_{T+1}
+  pred.LS <- matrix(NA, nrow=B, ncol=1)
+  pred.LA <- matrix(NA, nrow=B, ncol=1)
+  
+  for(b in 1:B){
+    # Find consequetive starting points for the resampling
+    idx <- sample(1:(T-p+1), 1, replace = TRUE)
+    x0 <- x[idx:(idx+p-1)]
+  
+    # Resample residuals and x
+    resample.resid.LS <- sample(resid.LS, replace = TRUE)
+    resample.resid.LA <- sample(resid.LA, replace = TRUE)
+    resample.x.LS <- ARp.filter(x0, beta$LS, resample.resid.LS)
+    resample.x.LA <- ARp.filter(x0, beta$LA, resample.resid.LA)
+    
+    # Store new estimates for residuals
+    pred.LS[b,] <- ARp.beta.est(resample.x.LS, p)$LS %*% x[(T-p+1):T] + sample(resample.resid.LS,1)
+    pred.LA[b,] <- ARp.beta.est(resample.x.LA, p)$LS %*% x[(T-p+1):T] + sample(resample.resid.LA,1)
+  }
+  return(list(LS=pred.LS, LA=pred.LA))
+}
+
+B <- 2000
+set.seed(4200)
+preds <- ARp.pred.bootstrap(x, p, B)
+
+quantile(preds$LS, probs = c(0.025, 0.975))
+quantile(preds$LA, probs = c(0.025, 0.975))
+
+
+
+
+
+
+
 
