59ec96b5ca8053bb45b6ecca2fa63e26c369ab57
Martin Gudahl Tufte <martingudahltufte@hotmail.com>
2022-04-26 11:45:24 +0200

    Finished the EM-algorithm.

--- ex3/problemC.R
+++ ex3/problemC.R
@@ -10,7 +10,6 @@ source('files/probAdata.R')
 # implementation of the EM algorithm
 EM.alg = function(z, u, lambda0, lambda1, tol=1e-8) {
   n <- length(z)
-  print(n)
   err <- Inf
   lambda0.iter <- c(lambda0)
   lambda1.iter <- c(lambda1)
@@ -37,7 +36,24 @@ u = read.table('files/u.txt')[,1]
 # find the MLE of lambda_0 and lambda_1
 MLE = EM.alg(z, u, 1, 1)
 
+MLE$lambda0
+MLE$lambda1
 
 
-# TODO: create convergence plots of the MLE estimates
+library(latex2exp)
+
+
+# convergence plots of the MLE estimates
+convergence_lambda0 <- ggplot(data.frame(x=1:length(MLE$lambda0)-1, lam0=MLE$lambda0, lam1=MLE$lambda1), aes(x=x)) +
+  geom_line(aes(y=lam0), lwd=0.5, color='blue') + xlab(expression(Iteration~t)) + ylab(expression(lambda[0]^{(t)})) + theme_bw()
+ggsave("./figures/convergence_lambda0.pdf", plot = convergence_lambda0, width = 5, height = 3)
+
+convergence_lambda1 <- ggplot(data.frame(x=1:length(MLE$lambda0)-1, lam0=MLE$lambda0, lam1=MLE$lambda1), aes(x=x)) +
+  geom_line(aes(y=lam1), lwd=0.5, color='blue') + xlab(expression(Iteration~t)) + ylab(expression(lambda[1]^{(t)})) + theme_bw()
+ggsave("./figures/convergence_lambda1.pdf", plot = convergence_lambda1, width = 5, height = 3)
+
+
+
+
+
 
